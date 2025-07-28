966658e421fd4a39a14b7346cdbf31daa3f2947d
Gerard <gerard.eklu@yahoo.fr>
2025-07-28 16:45:39 -0400

    Add services section

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-// import type { NextConfig } from 'next';
+import type { NextConfig } from 'next';
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: {
-    buildActivity: false, // This disables the build activity indicator
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
